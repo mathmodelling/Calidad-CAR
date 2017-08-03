246ea01f04404dfbd0245d2a132b64de3f22c0a5
David Castelblanco Benavides <cbdavides@gmail.com>
2017-08-03 15:10:45 -0500

    Se implementan diferentes funciones para calcular puntos de intersección.

--- geometry.py
+++ geometry.py
@@ -1,6 +1,5 @@
 from qgis.core import *
 
-
 def buildPolygon(segements):
     origins, ends = [], []
 
@@ -15,3 +14,32 @@ def buildPolygon(segements):
 
 def is_inside(polygon, point):
     return polygon.contains(point)
+
+def intersectionPoints(layerA, layerB):
+    points = []
+    for featA in layerA.getFeatures():
+        for featB in layerB.getFeatures():
+            inter = intersection(featA.geometry(), featB.geometry())
+            if inter is not None:
+                points.append(inter)
+
+    qgsPoints = [QgsPoint(point) for point in points]
+    return qgsPoints
+
+def intersectionLayerGeometry(layer, geometry):
+    intersections = []
+    for feature in layer.getFeatures():
+
+        temp = intersection(feature.geometry(), geometry)
+        if temp is not None:
+            intersections.append(temp)
+
+    print intersections
+    return intersections[0]
+
+def intersection(A, B):
+    if A.intersects(B):
+        inter = A.intersection(B)
+        if inter.wkbType() == QGis.WKBPoint:
+            return inter.asPoint()
+    return None
