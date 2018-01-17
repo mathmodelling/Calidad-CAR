ef205c115e10e06dcaa5c9d8ebd1f1c4818af40b
David Castelblanco Benavides <cbdavides@gmail.com>
2018-01-17 18:25:57 -0500

    Se cambia la dependencia para manejar archivos de Excel.

--- src/spreadsheets.py
+++ src/spreadsheets.py
@@ -4,21 +4,20 @@
 	Módulo encargado de crear hojas de
 	cálculo con una plantilla determinada.
 '''
-import openpyxl
-import time
+import xlrd
+import xlwt
 
-BOLD_FONT = openpyxl.styles.Font(name='Calibri', bold=True)
+BOLD_FONT_XLWT = xlwt.Style.easyxf('font: bold on;')
 CONTAMINANTS = ['OD', 'DBO', 'NH4', 'NO2', 'NO3', 'TDS', 'GyA', 'Condt',
 	'DQO', 'Porg', 'Pdis', 'EC', 'TC', 'T', 'PH', 'TSS', 'SS']
 
-
 def create_sheet_dt(workbook, name, distances, hours, initial_value=None):
 	'''
-		Crea una hoja el el libro de excel distancia / tiempo.
+		Crea una hoja en el libro de excel distancia / tiempo.
 
 		:param workbook: Libro de Excel en el que se va a crear
 		la hoja.
-		:type workbook: openpyxl.Workbook
+		:type workbook: xlwt.Workbook
 
 		:param name: Nombre de la hoja.
 		:type name: str
@@ -32,26 +31,24 @@ def create_sheet_dt(workbook, name, distances, hours, initial_value=None):
 		:param initial_value: Valor inicial con el que llenar la hoja
 
 	'''
-	sheet = workbook.create_sheet(name)
-	sheet['A1'] = 'L'
-	sheet['A1'].font = BOLD_FONT
+	sheet = workbook.add_sheet(name)
+	sheet.write(0, 0, 'L', BOLD_FONT_XLWT)
 
 	# Crear columna de distancias
 	for i in xrange(len(distances)):
-		sheet.cell(row=(i+2), column=1).value = distances[i]
-		sheet.cell(row=(i+2), column=1).font = BOLD_FONT
+		sheet.write(i + 1, 0, distances[i], BOLD_FONT_XLWT)
 
 	# Crear la fila de tiempo
 	for i in xrange(0,  hours + 1):
-		sheet.cell(row=1, column=(i+2)).value = 3600 * i
-		sheet.cell(row=1, column=(i+2)).font = BOLD_FONT
+		sheet.write(0, i + 1, 3600 * i, BOLD_FONT_XLWT)
 
 	if initial_value is not None:
-		rows = sheet.max_row
-		cols = sheet.max_column
-		for i in xrange(2, rows + 1):
-			for j in xrange(2, cols + 1):
-				sheet.cell(row=i, column=j).value = initial_value
+		rows = len(distances)
+		cols = hours + 1
+
+		for i in xrange(1, rows + 1):
+			for j in xrange(1, cols + 1):
+				sheet.write(i, j, initial_value)
 
 def create_sheet_td(workbook, name, distances, hours, initial_value=None):
 	'''
@@ -59,7 +56,7 @@ def create_sheet_td(workbook, name, distances, hours, initial_value=None):
 
 		:param workbook: Libro de Excel en el que se va a crear
 		la hoja.
-		:type workbook: openpyxl.Workbook
+		:type workbook: xlwt.Workbook
 
 		:param name: Nombre de la hoja.
 		:type name: str
@@ -73,27 +70,24 @@ def create_sheet_td(workbook, name, distances, hours, initial_value=None):
 		:param initial_value: Valor inicial con el que llenar la hoja
 
 	'''
-	sheet = workbook.create_sheet(name)
-	sheet['A1'] = 'L'
-	sheet['A1'].font = BOLD_FONT
+	sheet = workbook.add_sheet(name)
+	sheet.write(0, 0, 'L', BOLD_FONT_XLWT)
 
-	# Crear columna de distancias
+	# Crear fila de distancias
 	for i in xrange(len(distances)):
-		sheet.cell(row=1, column=(i+2)).value = distances[i]
-		sheet.cell(row=1, column=(i+2)).font = BOLD_FONT
+		sheet.write(0, i + 1, distances[i], BOLD_FONT_XLWT)
 
-	# Crear la fila de tiempo
+	# Crear la columna de tiempo
 	for i in xrange(0,  hours + 1):
-		sheet.cell(row=(i+2), column=1).value = 3600 * i
-		sheet.cell(row=(i+2), column=1).font = BOLD_FONT
+		sheet.write(i + 1, 0, 3600 * i, BOLD_FONT_XLWT)
 
 	if initial_value is not None:
-		rows = sheet.max_row
-		cols = sheet.max_column
+		rows = hours + 1
+		cols = len(distances)
 
-		for i in xrange(2, rows + 1):
-			for j in xrange(2, cols + 1):
-				sheet.cell(row=i, column=j).value = initial_value	
+		for i in xrange(1, rows + 1):
+			for j in xrange(1, cols + 1):
+				sheet.write(i, j, initial_value)
 
 def create_sheet_dc(workbook, name, distances, initial_value=None):
 	'''
@@ -101,7 +95,7 @@ def create_sheet_dc(workbook, name, distances, initial_value=None):
 
 		:param workbook: Libro de Excel en el que se va a crear
 		la hoja.
-		:type workbook: openpyxl.Workbook
+		:type workbook: xlwt.Workbook
 
 		:param name: Nombre de la hoja.
 		:type name: str
@@ -112,27 +106,24 @@ def create_sheet_dc(workbook, name, distances, initial_value=None):
 		:param initial_value: Valor inicial con el que llenar la hoja
 
 	'''
-	sheet = workbook.create_sheet(name)
-	sheet['A1'] = 'L'
-	sheet['A1'].font = BOLD_FONT
-
-	# Crear columna de distancias
+	sheet = workbook.add_sheet(name)
+	sheet.write(0, 0, 'L', BOLD_FONT_XLWT)
+	
+	# Crear fila de distancias
 	for i in xrange(len(distances)):
-		sheet.cell(row=(i+2), column=1).value = distances[i]
-		sheet.cell(row=(i+2), column=1).font = BOLD_FONT
+		sheet.write(i + 1, 0, distances[i], BOLD_FONT_XLWT)
 
-	# Crear la fila de tiempo
+	# Crear la columna de contaminantes
 	for i in xrange(len(CONTAMINANTS)):
-		sheet.cell(row=1, column=(i+2)).value = CONTAMINANTS[i]
-		sheet.cell(row=1, column=(i+2)).font = BOLD_FONT
-
+		sheet.write(0, i + 1, CONTAMINANTS[i], BOLD_FONT_XLWT)
+	
 	if initial_value is not None:
-		rows = sheet.max_row
-		cols = sheet.max_column
+		rows = len(distances)
+		cols = len(CONTAMINANTS)
 
-		for i in xrange(2, rows + 1):
-			for j in xrange(2, cols + 1):
-				sheet.cell(row=i, column=j).value = initial_value
+		for i in xrange(1, rows + 1):
+			for j in xrange(1, cols + 1):
+				sheet.write(i, j, initial_value)
 
 def create_sheet_tc(workbook, name, hours, initial_value=None):
 	'''
@@ -151,27 +142,24 @@ def create_sheet_tc(workbook, name, hours, initial_value=None):
 		:param initial_value: Valor inicial con el que llenar la hoja
 
 	'''
-	sheet = workbook.create_sheet(name)
-	sheet['A1'] = 'L'
-	sheet['A1'].font = BOLD_FONT
+	sheet = workbook.add_sheet(name)
+	sheet.write(0, 0, 'L', BOLD_FONT_XLWT)
 
-	# Crear la fila de tiempo
+	# Crear la fila del tiempo
 	for i in xrange(0,  hours + 1):
-		sheet.cell(row=(i+2), column=1).value = 3600 * i
-		sheet.cell(row=(i+2), column=1).font = BOLD_FONT
+		sheet.write(i + 1, 0, 3600 * i, BOLD_FONT_XLWT)
 
-	# Crear columna de distancias
+	# Crear la columna de contaminantes
 	for i in xrange(len(CONTAMINANTS)):
-		sheet.cell(row=1, column=(i+2)).value = CONTAMINANTS[i]
-		sheet.cell(row=1, column=(i+2)).font = BOLD_FONT
+		sheet.write(0, i + 1, CONTAMINANTS[i], BOLD_FONT_XLWT)
 
 	if initial_value is not None:
-		rows = sheet.max_row
-		cols = sheet.max_column
+		rows = hours + 1
+		cols = len(CONTAMINANTS)
 
-		for i in xrange(2, rows + 1):
-			for j in xrange(2, cols + 1):
-				sheet.cell(row=i, column=j).value = initial_value
+		for i in xrange(1, rows + 1):
+			for j in xrange(1, cols + 1):
+				sheet.write(i, j, initial_value)
 
 def create_book(path, distances, hours, wd=None, sl=None):
 	"""
@@ -193,11 +181,7 @@ def create_book(path, distances, hours, wd=None, sl=None):
 		:tpye sl: Decimal
 
 	"""
-	book = openpyxl.Workbook()
-
-	# Eliminar hoja Sheet, que se crea automáticamente
-	tmp = book.get_sheet_by_name('Sheet')
-	book.remove_sheet(tmp)
+	book = xlwt.Workbook()
 
 	# Crear hoja WD
 	create_sheet_dt(book, 'WD', distances, hours, wd)
@@ -211,17 +195,67 @@ def create_book(path, distances, hours, wd=None, sl=None):
 	create_sheet_dc(book, 'IC', distances)
 
 	# Crear hojas de Sinks and Sources
-	cont = 1
 	for contaminante in CONTAMINANTS:
-		name = "S%d" % cont
-		create_sheet_td(book, name, distances, hours, 0.0)
-		cont += 1
+		create_sheet_dt(book, 'S'+contaminante, distances, hours, 0.0)
 
 	book.save(path)
+	return book
+
+class Error(Exception):
+	def __init__(self, name, r, c):
+		self.name = name
+		self.r = r
+		self.c = c
+	def __str__(self):
+		return "NaN : %s (%d, %d)" % (
+			self.name,
+			self.r,
+			self.c)
+
+def verify_sheet(sheet):
+	"""Verifica que una hoja de cálculo no tenga celdas vacias."""
+	rows = sheet.nrows
+	cols = sheet.ncols
+
+	for i in xrange(1, rows ):
+		for j in xrange(1, cols):
+
+			try:
+				float(sheet.cell_value(i, j))
+				continue
+			except:
+				raise Error(sheet.name, i + 1, j + 1)
+
+def verify_book(workbook):
+	"""Verifica que no exitan celdas vacias en todas las hojas del libro de excel."""
+	# Verificar hoja WD
+	verify_sheet(workbook.sheet_by_name(u'WD'))
+	# Verificar hoja SL
+	verify_sheet(workbook.sheet_by_name(u'SL'))
+	# Verificar hoja WV
+	verify_sheet(workbook.sheet_by_name(u'WV'))
+	# Verificar hoja BC
+	verify_sheet(workbook.sheet_by_name(u'BC'))
+	# Verificar hoja IC
+	verify_sheet(workbook.sheet_by_name(u'IC'))
+
+	# Verificar hojas de Sinks and Sources
+	for contaminante in CONTAMINANTS:
+		verify_sheet(workbook.sheet_by_name("S" + contaminante))
+
+def load_book(path):
+	# return openpyxl.load_workbook(path)
+	return xlrd.open_workbook(path)
+
 
 if __name__ == '__main__':
-	d = [x for x in xrange(11)]
-	book = create_book("sample.xlsx", d, 24, 1.0, 0.35)
+	# d = [0.00, 4.82, 9.63, 14.45, 19.26, 24.08,	28.89, 33.71, 38.52,
+	# 	43.34, 48.15, 52.97, 57.78,	62.60, 67.41, 72.23, 77.04, 81.86,
+	# 	86.67, 91.49, 96.30, 101.12, 105.93, 110.75, 115.56, 120.38, 
+	# 	125.19, 130.01,	134.82,	139.64,	144.45,	149.27,	154.08,	158.90]
+	# book = create_book("sample3.xls", d, 24, 1.0, 0.35)
+	book = load_book("sample2.xlsx")
+	verify_book(book)
 	
 
 
