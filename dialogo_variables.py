e530c2284d1ed83394999eb9b25c8bea3eb65751
David Castelblanco Benavides <cbdavides@gmail.com>
2018-02-03 10:39:10 -0500

    Se agregan nuevas variables, se eliminan algunas variables, y se rediseña la interfaz

--- dialogo_variables.py
+++ dialogo_variables.py
@@ -12,14 +12,15 @@ RUTA_ARCHIVO_VARIABLES = os.path.join(
 RUTA_ARCHIVO_VARIABLES_LOCK = os.path.join(
     os.path.dirname(__file__), 'data', 'variables.lock.txt')
 
-siglas = ['D', 'Da', 'ko2', 'cs', 'knh3', 'ksnh3', 'alfa_nh3',
+siglas = ['Da', 'ko2', 'cs', 'knh3', 'ksnh3', 'alfa_nh3',
 	'kdbo', 'ks', 'alfa_no2', 'ksod', 'knt', 'NT', 'kno2', 'kno3',
     'kDQO', 'kTDS', 'A', 'alfa_1', 'miu', 'F', 'kTC', 'teta_TC',
-    'kEC', 'teta_EC', 'Jdbw', 'qtex', 'kN', 'kH', 'H', 'kOH', 'OH',
-    'fdw', 'kf', 'kb', 'kv', 'Cg', 'Henry', 'R', 'T', 'alfa_2', 'resp',
+    'kEC', 'teta_EC', 'Jdbw', 'qtex', 'kN', 'kH', 'kOH', 'fdw', 
+    'kf', 'kb', 'kv', 'Cg', 'Henry', 'R', 'T', 'alfa_2', 'resp',
     'kPorg', 'kPsed', 'sigma2', 'Ws', 'Rs', 'Rp', 'k', 'den', 'Cp', 
     'teta_DBO', 'teta_NH3', 'teta_NO2', 'teta_DQO', 'teta_NT', 'teta_NO3', 
-    'Kw', 'K1', 'K2', 'Vv', 'As', 'CO2S', 'Wrp', 'FrH', 'Diff']
+    'Kw', 'K1', 'K2', 'Vv', 'As', 'CO2S', 'Wrp', 'FrH', 'Diff',
+    'As1', 'Jsn', 'sbc', 'Tair', 'Aair', 'eair', 'RL', 'Uw', 'es']
 
 class VarsDialog(QtGui.QDialog, FORM_CLASS):
     def __init__(self, parent = None):
@@ -27,7 +28,7 @@ class VarsDialog(QtGui.QDialog, FORM_CLASS):
         super(VarsDialog, self).__init__(parent)
         self.setupUi(self)
         self.dic = {
-            'D': self.D, 'Da': self.Da, 'ko2': self.ko2, 'cs': self.cs, 
+            'Da': self.Da, 'ko2': self.ko2, 'cs': self.cs, 
             'knh3': self.knh3, 'ksnh3': self.ksnh3, 'alfa_nh3': self.alfa_nh3 ,
             'kdbo': self.kdbo, 'ks': self.ks, 'alfa_no2': self.alfa_no2, 
             'ksod': self.ksod, 'knt': self.knt, 'NT': self.NT, 'kno2': self.kno2, 
@@ -35,7 +36,7 @@ class VarsDialog(QtGui.QDialog, FORM_CLASS):
             'alfa_1': self.alfa_1, 'miu': self.miu, 'F': self.F, 'kTC': self.kTC, 
             'teta_TC': self.teta_TC, 'kEC': self.kEC, 'teta_EC': self.teta_EC, 
             'Jdbw': self.Jdbw, 'qtex': self.qtex, 'kN': self.kN, 'kH': self.kH, 
-            'H': self.H, 'kOH': self.kOH, 'OH': self.OH, 'fdw': self.fdw, 'kf': self.kf, 
+            'kOH': self.kOH, 'fdw': self.fdw, 'kf': self.kf, 
             'kb': self.kb, 'kv': self.kv, 'Cg': self.Cg, 'Henry': self.Henry, 
             'R': self.R, 'T': self.T, 'alfa_2': self.alfa_2, 'resp': self.resp,
             'kPorg': self.kPorg, 'kPsed': self.kPsed, 'sigma2': self.sigma2, 'Ws': self.Ws, 
@@ -43,7 +44,9 @@ class VarsDialog(QtGui.QDialog, FORM_CLASS):
             'teta_DBO': self.teta_DBO, 'teta_NH3': self.teta_NH3, 'teta_NO2': self.teta_NO2, 
             'teta_DQO': self.teta_DQO, 'teta_NT': self.teta_NT, 'teta_NO3': self.teta_NO3, 
             'Kw': self.Kw, 'K1': self.K1, 'K2': self.K2, 'Vv': self.Vv, 'As': self.As, 
-            'CO2S': self.CO2S, 'Wrp': self.Wrp, 'FrH': self.FrH, 'Diff': self.Diff}
+            'CO2S': self.CO2S, 'Wrp': self.Wrp, 'FrH': self.FrH, 'Diff': self.Diff,
+            'As1': self.As1 , 'Jsn': self.Jsn , 'sbc': self.sbc , 'Tair': self.Tair , 
+            'Aair': self.Aair , 'eair': self.eair , 'RL': self.RL , 'Uw': self.Uw , 'es': self.es }
         
         # Agregar validador
         validator = QtGui.QDoubleValidator()
